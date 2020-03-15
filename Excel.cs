22369aced8036ec625def2cd128113d77d017189
Przemysałw Handzel <lewusek166@gmail.com>
2020-03-15 19:00:14 +0100

    zaczynamy

---
+++ Excel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace RimOptiList
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = excel.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i,int j)
+        {
+            i++;
+            j++;
+            if(ws.Cells[i, j].Value2!=null)
+            {
+                return ws.Cells[i, j].Value2;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void WriteToCell(int i, int j, string s)
+        {
+            i++;
+            j++;
+            ws.Cells[i, j].Value2 = s;
+        }
+        public void SaveData()
+        {
+            wb.Save();
+        }
+        public void SaveAsData(string path)
+        {
+            wb.SaveAs(path);
+        }
+        public void Close()
+        {
+            wb.Close();
+        }
+    }
+}
